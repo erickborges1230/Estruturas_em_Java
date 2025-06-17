5348e07c803127f92b3bf75e4ee92247eb2e3efd
Erick Borges <erickborges1230@gmail.com>
2025-06-17 20:15:37 -0300

    First commit

---
+++ src/main/java/org/estruras/MaiorMenor.java
@@ -0,0 +1,22 @@
+package org.estruras;
+
+import java.util.Scanner;
+public class MaiorMenor {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int menor, maior, numero,i;
+        numero = teclado.nextInt();
+        maior=numero;
+        menor=numero;
+        for (i=2; i<10; i++){
+            numero = teclado.nextInt();
+            if(numero<menor){
+                menor = numero;
+            }
+            else if (numero>maior) {
+                maior=numero;
+            }
+        }
+        System.out.println("O menor número informado foi "+menor+" e o maior número informado foi "+maior);
+    }
+}
--- src/main/java/org/estruras/NumeroDescrescente.java
+++ src/main/java/org/estruras/NumeroDescrescente.java
@@ -1,4 +1,17 @@
 package org.estruras;
 
+import java.util.Scanner;
+
 public class NumeroDescrescente {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Favor informar o número ");
+        numero = teclado.nextInt();
+
+        for (int i = numero; i>=1; i=i-1 ){
+            System.out.println("Numero = "+i);
+        }
+    }
 }
