edb0abcd2054a8227c9caa9a7b244276f8eeb02e
Erick Borges <erickborges1230@gmail.com>
2025-06-20 23:51:59 -0300

    First commit

---
+++ src/main/java/org/estruras/Numeros_Pares.java
@@ -0,0 +1,19 @@
+package org.estruras;
+
+import java.util.Scanner;
+
+public class Numeros_Pares {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Favor informar o número A");
+        int a = teclado.nextInt();
+
+        System.out.println("Favor informar o número B");
+        int b = teclado.nextInt();
+
+        for(int i=a+1; i<b; i++){ //Neste caso não está incluindo nem o A e nem o B.
+            if(i%2==0)
+                System.out.println("O número: "+i+" é par");
+        }
+    }
+}
