2b089a75a567039390f5933e54c0fbd31a9a04f8
Erick Borges <erickborges1230@gmail.com>
2025-06-21 14:08:55 -0300

    First commit

---
+++ src/main/java/Taxa_de_Acrescimento_AB.java
@@ -0,0 +1,19 @@
+public class Taxa_de_Acrescimento_AB {
+    public static void main(String[] args) {
+        //Declaração de constantes
+        final double habitantesA = 5000000;
+        final double habitantesB = 7000000;
+        final double taxaA  = 0.03;
+        final double taxaB  = 0.02;
+        int tempo = 0;
+        double populacaoA = habitantesA;
+        double populacaoB = habitantesB;
+        while (populacaoB >= populacaoA)
+        {
+            populacaoB = (populacaoB + populacaoB * taxaB);
+            populacaoA = (populacaoA + populacaoA * taxaA);
+            tempo++;
+        }
+        System.out.println("A população de A levará "+tempo+" anos para superar a população de B");
+    }
+}
