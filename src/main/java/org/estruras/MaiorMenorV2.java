e323745a3e6276a52365e81519bdcebd92685570
Erick Borges <erickborges1230@gmail.com>
2025-06-20 23:27:35 -0300

    First commit

---
+++ src/main/java/org/estruras/MaiorMenorV2.java
@@ -0,0 +1,25 @@
+package org.estruras;
+
+import java.util.Scanner;
+
+public class MaiorMenorV2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int menor, maior, numero,i;
+        System.out.println("Favor informar o valor");
+        numero = teclado.nextInt();
+        maior=numero;
+        menor=numero;
+
+        do {
+            if (numero < menor)
+                menor = numero;
+            else if (numero > maior)
+                maior = numero;
+                System.out.println("Favor informar o valor");
+                numero = teclado.nextInt();
+        }
+        while (numero != 0) ;
+        System.out.println("O menor número informado foi " + menor + " e o maior número informado foi " + maior);
+    }
+}
